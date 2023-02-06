54d6415834583fbf52531aa1b1e091f1dd213908
RoDmitry <18184039+RoDmitry@users.noreply.github.com>
2023-02-06 20:25:08 +0000

    Save json

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,3 +1,5 @@
+use crate::encom_from_json;
+
 mod bytes;
 mod int;
 mod option_int;
@@ -28,3 +30,56 @@ fn stream() {
     // If err.is_eof(), can join the remaining data to new data and continue.
     let remaining = &data[stream.byte_offset()..];
 } */
+
+#[test]
+fn big_json() {
+    let v = encom_from_json!({
+        "web-app": {
+            "servlet": [
+                {
+                    "init-param": {
+                        "adminGroupID": 4,
+                        "betaServer": true,
+                        "dataLog": 1,
+                        "dataLogMaxSize": "",
+                        "fileTransferFolder": "fileTransferFolder",
+                        "log": 1
+                    },
+                    "servlet-name": "cofaxTools"
+                },
+                {
+                    "init-param": {
+                        "cacheTemplatesStore": null,
+                        "cacheTemplatesTrack": 100,
+                        "configGlossary:poweredBy": "Cofax",
+                        "dataStoreMaxConns": 100,
+                        "dataStoreName": "cofax",
+                        "maxUrlLength": 500,
+                        "templateOverridePath": "",
+                        "templatePath": "templates",
+                        "useDataStore": true,
+                        "useJSP": false
+                    },
+                    "servlet-class": "org.cofax.cds.CDSServlet",
+                    "servlet-name": "cofaxCDS"
+                },
+                {
+                    "init-param": {
+                        "mailHost": "mail1",
+                        "mailHostOverride": "mail2"
+                    },
+                    "servlet-name": "cofaxEmail"
+                },
+            ],
+            "servlet-mapping": {
+                "cofaxTools": "/tools/*",
+                "fileServlet": "/static/*"
+            },
+            "taglib": {
+                "taglib-location": "/WEB-INF/tlds/cofax.tld"
+            }
+        }
+    });
+
+    println!("{v:#}");
+}
